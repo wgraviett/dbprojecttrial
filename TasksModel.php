a5f7da9a39d6ee4d230bec8c8684a4c927408f00
Wesley Graviett <wgraviett@gmail.com>
2018-04-17 00:11:34 -0500

    Login setup and edit form.

---
+++ TasksModel.php
@@ -0,0 +1,309 @@
+<?php
+
+	// NOTE: this version has beginning of support for logging in and supporting
+	// multiple users.  This version does not yet have the database table for users
+	// or the change to the database so that each task is affiliated with a user.
+
+	require('User.php');
+
+	class TasksModel {
+		private $error = '';
+		private $mysqli;
+		private $orderBy = 'title';
+		private $orderDirection = 'asc';
+		private $user;
+		
+		public function __construct() {
+			session_start();
+			$this->initDatabaseConnection();
+			//$this->restoreOrdering();
+			$this->restoreUser();
+		}
+		
+		public function __destruct() {
+			if ($this->mysqli) {
+				$this->mysqli->close();
+			}
+		}
+		
+		public function getError() {
+			return $this->error;
+		}
+		
+		private function initDatabaseConnection() {
+			require('db_credentials.php');
+			$this->mysqli = new mysqli($servername, $username, $password, $dbname);
+			if ($this->mysqli->connect_error) {
+				$this->error = $mysqli->connect_error;
+			}
+		}
+		
+		private function restoreOrdering() {
+			$this->orderBy = $_SESSION['orderby'] ? $_SESSION['orderby'] : $this->orderBy;
+			$this->orderDirection = $_SESSION['orderdirection'] ? $_SESSION['orderdirection'] : $this->orderDirection;
+		
+			$_SESSION['orderby'] = $this->orderBy;
+			$_SESSION['orderdirection'] = $this->orderDirection;
+		}
+		
+		private function restoreUser() {
+			if ($loginID = $_SESSION['loginid']) {
+				$this->user = new User();
+				if (!$this->user->load($loginID, $this->mysqli)) {
+					$this->user = null;
+				}
+			}
+		}
+		
+		public function getUser() {
+			return $this->user;
+		}
+		
+		public function login($loginID, $password) {
+			// check if loginID and password are valid by comparing
+			// encrypted version of password to encrypted password stored
+			// in database for user with loginID
+			
+			$user = new User();
+			if ($user->load($loginID, $this->mysqli) && password_verify($password, $user->hashedPassword)) {
+				$this->user = $user;
+				$_SESSION['loginid'] = $loginID;
+				return array(true, "");
+			} else {
+				$this->user = null;
+				$_SESSION['loginid'] = '';
+				return array(false, "Invalid login information.  Please try again.");
+			}
+		}
+		
+		public function logout() {
+			$this->user = null;
+			$_SESSION['loginid'] = '';
+		}
+	
+		public function toggleOrder($orderBy) {
+			if ($this->orderBy == $orderBy)	{
+				if ($this->orderDirection == 'asc') {
+					$this->orderDirection = 'desc';
+				} else {
+					$this->orderDirection = 'asc';
+				}
+			} else {
+				$this->orderDirection = 'asc';
+			}
+			$this->orderBy = $orderBy;
+			
+			$_SESSION['orderby'] = $this->orderBy;
+			$_SESSION['orderdirection'] = $this->orderDirection;			
+		}
+		
+		public function getOrdering() {
+			return array($this->orderBy, $this->orderDirection);
+		}
+		
+		public function getTasks() {
+			$this->error = '';
+			$tasks = array();
+			
+			if (!$this->user) {
+				$this->error = "User not specified. Unable to get task.";
+				return $this->error;
+			}
+		
+			if (! $this->mysqli) {
+				$this->error = "No connection to database.";
+				return array($tasks, $this->error);
+			}
+		
+			//$orderByEscaped = $this->mysqli->real_escape_string($this->orderBy);
+			//$orderDirectionEscaped = $this->mysqli->real_escape_string($this->orderDirection);
+			$userIDEscaped = $this->mysqli->real_escape_string($this->user->userID);
+
+			$sql = "SELECT Applications.id,Applications.StudentID, users.First_Name, users.Last_Name, Applications.application_status, Applications.ProgramID
+FROM Applications INNER JOIN users ON users.studentID = Applications.studentID";
+			if ($result = $this->mysqli->query($sql)) {
+				if ($result->num_rows > 0) {
+					while($row = $result->fetch_assoc()) {
+						array_push($tasks, $row);
+					}
+				}
+				$result->close();
+			} else {
+				$this->error = $mysqli->error;
+			}
+			
+			return array($tasks, $this->error);
+		}
+		
+		public function getTask($id) {
+			$this->error = '';
+			$task = null;
+			
+			if (!$this->user) {
+				$this->error = "User not specified. Unable to get task.";
+				return $this->error;
+			}
+		
+			if (! $this->mysqli) {
+				$this->error = "No connection to database.";
+				return array($task, $this->error);
+			}
+			
+			if (! $id) {
+				$this->error = "No id specified for task to retrieve.";
+				return array($task, $this->error);
+			}
+			
+			$idEscaped = $this->mysqli->real_escape_string($id);
+			$userIDEscaped = $this->mysqli->real_escape_string($this->user->userID);
+
+			$sql = "SELECT Applications.id, Applications.studentID, users.First_Name, users.Last_Name
+FROM users INNER JOIN Applications ON Applications.studentID = users.studentid
+WHERE Applications.id ='$idEscaped'";
+			if ($result = $this->mysqli->query($sql)) {
+				if ($result->num_rows > 0) {
+					$task = $result->fetch_assoc();
+				}
+				$result->close();
+			} else {
+				$this->error = $this->mysqli->error;
+			}
+			
+			return array($task, $this->error);		
+		}
+		
+		public function addTask($data) {
+			$this->error = '';
+			
+			if (!$this->user) {
+				$this->error = "User not specified. Unable to add task.";
+				return $this->error;
+			}
+			
+			$title = $data['title'];
+			$category = $data['category'];
+			$description = $data['description'];
+			
+			if (! $title) {
+				$this->error = "No title found for task to add. A title is required.";
+				return $this->error;			
+			}
+			
+			if (! $category) {
+				$category = 'uncategorized';
+			}
+			
+			$titleEscaped = $this->mysqli->real_escape_string($title);		
+			$categoryEscaped = $this->mysqli->real_escape_string($category);
+			$descriptionEscaped = $this->mysqli->real_escape_string($description);
+			$userIDEscaped = $this->mysqli->real_escape_string($this->user->userID);
+
+			$sql = "INSERT INTO tasks (title, description, category, addDate, UserID) VALUES ('$titleEscaped', '$descriptionEscaped', '$categoryEscaped', NOW(), '$userIDEscaped')";
+	
+			if (! $result = $this->mysqli->query($sql)) {
+				$this->error = $this->mysqli->error;
+			}
+			
+			return $this->error;
+		}
+		
+		public function updateTaskCompletionStatus($id, $status) {
+			$this->error = "";
+			
+			if (!$this->user) {
+				$this->error = "User not specified. Unable to update task completion status.";
+				return $this->error;
+			}
+		
+			$completedDate = 'null';
+			if ($status == 'completed') {
+				$completedDate = 'NOW()';
+			}
+	
+			if (!$id) {
+				$this->error = "No task was specified to change completion status.";
+			} else {
+				$idEscaped = $this->mysqli->real_escape_string($id);
+				$userIDEscaped = $this->mysqli->real_escape_string($this->user->userID);
+				$sql = "UPDATE tasks SET completedDate = $completedDate WHERE userID = $userIDEscaped AND id = '$idEscaped'";
+				if (! $result = $this->mysqli->query($sql) ) {
+					$this->error = $this->mysqli->error;
+				}
+			}
+	
+			return $this->error;
+		}
+		
+		public function updateTask($data) {
+			$this->error = '';
+			
+			if (!$this->user) {
+				$this->error = "User not specified. Unable to update task.";
+				return $this->error;
+			}
+			
+			if (! $this->mysqli) {
+				$this->error = "No connection to database. Unable to update task.";
+				return $this->error;
+			}
+			
+			$id = $data['id'];
+			if (! $id) {
+				$this->error = "No id specified for task to update.";
+				return $this->error;			
+			}
+			
+			$title = $data['title'];
+			if (! $title) {
+				$this->error = "No title found for task to update. A title is required.";
+				return $this->error;			
+			}		
+			
+			$description = $data['description'];
+			$category = $data['category'];
+			
+			$idEscaped = $this->mysqli->real_escape_string($id);
+			$titleEscaped = $this->mysqli->real_escape_string($title);
+			$descriptionEscaped = $this->mysqli->real_escape_string($description);
+			$categoryEscaped = $this->mysqli->real_escape_string($category);
+			$userIDEscaped = $this->mysqli->real_escape_string($this->user->userID);
+			$sql = "UPDATE tasks SET title='$titleEscaped', description='$descriptionEscaped', category='$categoryEscaped' WHERE userID = $userIDEscaped AND id = $idEscaped";
+			if (! $result = $this->mysqli->query($sql) ) {
+				$this->error = $this->mysqli->error;
+			} 
+			
+			return $this->error;
+		}
+		
+		public function deleteTask($id) {
+			$this->error = '';
+			
+			if (!$this->user) {
+				$this->error = "User not specified. Unable to delete task.";
+				return $this->error;
+			}
+			
+			if (! $this->mysqli) {
+				$this->error = "No connection to database. Unable to delete task.";
+				return $this->error;
+			}
+			
+			if (! $id) {
+				$this->error = "No id specified for task to delete.";
+				return $this->error;			
+			}			
+		
+			$idEscaped = $this->mysqli->real_escape_string($id);
+			$userIDEscaped = $this->mysqli->real_escape_string($this->user->userID);
+			$sql = "DELETE FROM tasks WHERE userID = $userIDEscaped AND id = $idEscaped";
+			if (! $result = $this->mysqli->query($sql) ) {
+				$this->error = $this->mysqli->error;
+			}
+			
+			return $this->error;
+		}
+
+	
+	}
+
+?>
---
+++ TasksViews.php
@@ -0,0 +1,192 @@
+<?php
+
+	class TasksViews {
+		private $stylesheet = 'taskmanager.css';
+		private $pageTitle = 'Tasks';
+		
+		public function __construct() {
+
+		}
+		
+		public function __destruct() {
+
+		}
+		
+		public function taskListView($user, $tasks, /*$orderBy = 'title', $orderDirection = 'asc',*/ $message = '') {
+			$body = "<h1>Applications for {$user->firstName} {$user->lastName}</h1>\n";
+		
+			if ($message) {
+				$body .= "<p class='message'>$message</p>\n";
+			}
+		
+			$body .= "<p><a class='taskButton' href='index.php?view=taskform'>+ Add Task</a> <a class='taskButton' href='index.php?logout=1'>Logout</a></p>\n";
+	
+			if (count($tasks) < 1) {
+				$body .= "<p>No Applications to display!</p>\n";
+				return $this->page($body);
+			}
+	
+			$body .= "<table>\n";
+			$body .= "<tr><th>delete</th><th>edit</th><th>View</th>";
+		
+			$columns = array(array('name' => 'id', 'label' => 'Application ID'),
+			array('name' => 'First_Name', 'label' => 'First Name'),
+							array('name' => 'Last_Name', 'label' => 'Last Name'),
+							array('name' => 'StudentID', 'label' => 'StudentID'), 
+							 array('name' => 'application_status', 'label' => 'Application Status'), 
+							 array('name' => 'ProgramID', 'label' => 'ProgramID'));
+		
+			// geometric shapes in unicode
+			// http://jrgraphix.net/r/Unicode/25A0-25FF
+			foreach ($columns as $column) {
+				$name = $column['name'];
+				$label = $column['label'];
+				if ($name == $orderBy) {
+					if ($orderDirection == 'asc') {
+						$label .= " &#x25BC;";  // ▼
+					} else {
+						$label .= " &#x25B2;";  // ▲
+					}
+				}
+				$body .= "<th><a class='order' href='index.php?orderby=$name'>$label</a></th>";
+			}
+	
+			foreach ($tasks as $task) {
+				$id = $task['id'];
+				$application_status = $task['application_status'];
+				$ProgramID = $task['ProgramID'];
+				$First_Name= $task['First_Name'];
+				$Last_Name = $task['Last_Name'];
+				$StudentID = $task['StudentID'];
+
+
+			
+				$body .= "<tr>";
+				$body .= "<td><form action='index.php' method='post'><input type='hidden' name='action' value='delete' /><input type='hidden' name='id' value='$id' /><input type='submit' value='Delete'></form></td>";
+				$body .= "<td><form action='index.php' method='post'><input type='hidden' name='action' value='edit' /><input type='hidden' name='id' value='$id' /><input type='submit' value='Edit'></form></td>";
+				$body .= "<td><form action='index.php' method='post'><input type='hidden' name='action' value='view' /><input type='hidden' name='id' value='$id' /><input type='submit' value='View'></form></td>";
+				$body .= "<td>$id</td><td>$First_Name</td><td>$Last_Name</td><td>$StudentID</td><td>$application_status</td><td>$ProgramID</td>";
+				$body .= "</tr>\n";
+			}
+			$body .= "</table>\n";
+	
+			return $this->page($body);
+		}
+		
+		public function taskFormView($user, $data = null, $message = '') {
+			$FirstName = '';
+			$LastName ='';
+			$StudentID='';
+			$Program='';
+			$Answer_1='';
+		$Program_Selected = array('Uncategorized' => '','MSN_AGNP' => '', 'MSN_FNP' => '', 'MSN_PNP-ACPNP' => '', 'MSN_PMHNP' => '');
+
+			
+			
+			
+					if ($data){
+				$Program=$data['ProgramID'] ? $data['ProgramID'] : 'uncategorized';
+				$FirstName = $data['First_Name'];
+				$LastName = $data['Last_Name'];
+				$StudentID= $data['studentID'];
+				$Program_Selected[$Program] = 'selected'; //check this 
+				$Answer_1 = $data['application_question_1'];
+			} else {
+				$Program_Selected['uncategorized'] = 'selected';
+			}
+	
+			$body = "<h1>Applications for for {$user->FirstName} {$user->LastName}</h1>\n";
+
+			if ($message) {
+				$body .= "<p class='message'>$message</p>\n";
+			}
+		
+			$body .= "<form action='index.php' method='post'>";
+		
+			if ($data['id']) {
+				$body .= "<input type='hidden' name='action' value='update' />";
+				$body .= "<input type='hidden' name='id' value='{$data['id']}' />";
+			} else {
+				$body .= "<input type='hidden' name='action' value='add' />";
+			}
+		
+			$body .= <<<EOT2
+ <p>Please fill out the form below<br />
+<p>Student ID</>
+<input type = "number" name="StudentID" value="$StudentID" placeholder ="########" maxlength ="8" size="80"></p>
+ <p> Full Legal Name<br />
+<label for=LastName>Last Name</label>
+  <input type="text" name="LastName" value="$LastName" placeholder="Last Name" maxlength="255" size="20"></p>
+<label for=FirstName>First Name</label>
+  <input type="text" name="FirstName" value="$FirstName" placeholder="First Name" maxlength="255" size="20"></p>
+<label for=program>Select Program</label>
+  <select name="Program">
+  	  <option value="0">Uncategorized</option>
+	  <option value="1">MSN - Adult Gerontology NP</option>
+	  <option value="2">MSN - Family Nurse Practitioner</option>
+	  <option value="3">MSN - Pediatric Nurse Practitioner</option>
+	  <option value="4" selected>MSN - Psychiatric Mental Health Nurse Practitioner</option>
+  </select>
+</p>
+  <label for=Answer_1>What was your favorite undergraduate nursing course?  </label>
+  </br>
+  <textarea name="Answer_1" rows="6" cols="80" placeholder="">$Answer_1</textarea></p>
+  <input type="submit" value="Submit">
+</form>
+EOT2;
+
+			return $this->page($body);
+		}
+		
+		
+		public function loginFormView($data = null, $message = '') {
+			$loginID = '';
+			if ($data) {
+				$loginID = $data['loginid'];
+			}
+		
+			$body = "<h1>Tasks</h1>\n";
+			
+			if ($message) {
+				$body .= "<p class='message'>$message</p>\n";
+			}
+			
+			$body .= <<<EOT
+<form action='index.php' method='post'>
+<input type='hidden' name='action' value='login' />
+<p>User ID<br />
+  <input type="text" name="loginid" value="$loginID" placeholder="login id" maxlength="255" size="80"></p>
+<p>Title<br />
+  <input type="password" name="password" value="" placeholder="password" maxlength="255" size="80"></p>
+  <input type="submit" name='submit' value="Login">
+</form>	
+EOT;
+			
+			return $this->page($body);
+		}
+		
+		public function errorView($message) {	
+			$body = "<h1>Tasks</h1>\n";
+			$body .= "<p>$message</p>\n";
+			
+			return $this->page($body);
+		}
+		
+		private function page($body) {
+			$html = <<<EOT
+<!DOCTYPE html>
+<html>
+<head>
+<title>{$this->pageTitle}</title>
+<link rel="stylesheet" type="text/css" href="{$this->stylesheet}">
+</head>
+<body>
+$body
+<p>&copy; 2017 Dale Musser. All rights reserved.</p>
+</body>
+</html>
+EOT;
+			return $html;
+		}
+
+}
